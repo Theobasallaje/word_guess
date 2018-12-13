06284ac8789222a39801739e462efe02193dd350
Theo <theobasallaje@gmail.com>
2018-12-13 14:54:28 -0600

    added dashes and compare functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,68 @@
- // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
- var randomArray = ["Superman", "Batman", "The Flash", "Martian Man Hunter", "Wonder Woman"];
- var guesses = [];
+ // Creates an array that lists out all of the words
+//  var randomArray = ["Superman", "Batman", "The Flash", "Martian Man Hunter", "Wonder Woman"];
+ var randomArray = ["rock", "paper", "scissors"];
+ var guessesArray = [];
+ var dashes = [];
 
- // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+ // Creating variables. They start at 0.
  var wins = 0;
  var guessCount = 0;
 
  // Create variables that hold references to the places in the HTML where we want to display things.
-//  var directionsText = document.getElementById("directions-text");
+ var directionsText = document.getElementById("directions");
  var randomWords = document.getElementById("randomWord");
  var wins = document.getElementById("wins");
  var guessCount = document.getElementById("guessCount");
+ var guessElement = document.getElementById("userGuess");
+
+     // Randomly generates a word from the array.
+function randomWordGen(){
+        var randomWord = randomArray[Math.floor(Math.random() * randomArray.length)];
+        return randomWord;
+        //console.log(randomWord);
+}
+
+function dashDisplay(randomWord){
+    //var dashes = "";
+    for (var i=0; i<randomWord.length; i++){
+        dashes.push("-");
+        //dashes += "-";
+   }
+   randomWords.textContent = dashes.join(' ');
+}
+
+function guesses(userGuess){
+    guessesArray.push(userGuess);
+    console.log(guessesArray);
+    guessElement.textContent = guessesArray.join(" "); //try append or add you guessed to the textContent
+}
+
+function compareWords(randomWord, userGuess){
+        for (var i=0; i<randomWord.length; i++){
+            console.log(randomWord[i]);
+           if (userGuess===randomWord[i]){
+               console.log(userGuess+"  "+randomWord[i]);
+               ///replace the dash with this letter
+               dashes[i] = userGuess;
+               randomWords.textContent = dashes.join(' ');
+           }
+        }
+}
+
+var rand = randomWordGen();
+dashDisplay(rand);
 
  // This function is run whenever the user presses a key.
  document.onkeyup = function (event) {
 
      // Determines which key was pressed.
      var userGuess = event.key;
+     console.log("user guess: " + userGuess);
 
-     // Randomly generates a word from the array.
-     function randomWordGen(){
-        var randomWord = randomArray[Math.floor(Math.random() * randomArray.length)];
-        return randomWord;
-        //console.log(randomWord);
-     }  
+    compareWords(rand, userGuess);
+    guesses(userGuess);
 
-     function compareWords(randomWord){
-         for (i=0; i<randomWord.length; i++){
-             console.log(randomWord[i]);
-         }
-     }
-     var rand = randomWordGen();
-
-     compareWords(rand);
-        // for (i=0; i<randomWord.length; i++){
-        //     if (userGuess===randomWord[i]){
-        //         console.log(randomWord);
-        //         //guessCount++;
-        //     }
-        //     else {
-        //         guesses.push(userGuess);
-        //         console.log(randomWord);
-        //         //guessCount++;
-        //     }
-        // }
-         // Hide the directions
-         directions.textContent = "";
+    directionsText.textContent = "";
 
          // Display the user and computer guesses, and wins/losses/ties.
         //  userGuess.textContent = "You chose: " + guesses;
